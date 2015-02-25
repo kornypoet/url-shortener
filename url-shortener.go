69f64b7c797c41feea4d00e30a1716146e2ddd1f
Travis Dempsey <dempsey.travis@gmail.com>
2015-02-25 11:14:15 -0600

    code is completely functional

--- url-shortener.go
+++ url-shortener.go
@@ -20,7 +20,6 @@ type Payload struct {
   Count int
 }
 
-
 func main() {
 
   mongo, err := mgo.Dial("localhost")
@@ -36,8 +35,23 @@ func main() {
   router := gin.Default()
   router.Use(gin.Logger())
 
-  router.GET("/" , func(c *gin.Context) {
-    c.String(200, "Hello World")
+  router.GET("/status" , func(c *gin.Context) {
+    c.String(200, "OK")
+  })
+
+  router.GET("/shorten/:id", func(c *gin.Context) {
+    id     := c.Params.ByName("id")
+    result := Payload{}
+    change := mgo.Change{
+      Update: bson.M{"$inc": bson.M{"count": 1}},
+      ReturnNew: true,
+    }
+    _, err := coll.Find(bson.M{"_id": id}).Apply(change, &result)
+    if err != nil {
+      c.String(404, "Not Found")
+    } else {
+      c.Redirect(301, result.Url)
+    }
   })
 
   router.POST("/shorten", func(c *gin.Context) {
@@ -50,20 +64,20 @@ func main() {
       fmt.Println(err)
       c.String(400, "Malformed Url")
     } else {
-
       hasher := md5.New()
       hasher.Write([]byte(json.Url))
-      id := hex.EncodeToString(hasher.Sum(nil))
-
+      id     := hex.EncodeToString(hasher.Sum(nil))
       result := Payload{}
-      err = coll.Find(bson.M{"_id": id}).One(&result)
+      err    := coll.Find(bson.M{"_id": id}).One(&result)
       if err != nil {
-        fmt.Println(err)
-
-        err = coll.Insert(&Payload{id, json.Url, 1})
+        fmt.Println("Record not found: creating new entry")
+        result = Payload{id, json.Url, 1}
+        err = coll.Insert(&result)
         if err != nil {
           fmt.Println(err)
         }
+      } else {
+        fmt.Println("Found result " + result.Url)
       }
 
       var msg struct {
@@ -73,8 +87,8 @@ func main() {
       }
 
       msg.Path  = id
-      msg.Host  = url.Host
-      msg.Count = 1
+      msg.Host  = result.Url
+      msg.Count = result.Count
 
       c.JSON(200, msg)
     }
